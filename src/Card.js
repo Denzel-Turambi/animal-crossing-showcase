4383806802b81a4575bc0fe35be90394320e2889
Denzel Turambi <dturambi26@gmail.com>
2023-08-03 22:29:00 -0600

    feat: Add styling to focus card

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,14 @@
 import './Card.css';
-import { Link } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
 function Card({name, img, id}) {
 
   return (
-    <Link to={`/${name}`}>
+    <NavLink to={`/${name}`}>
     <div className="card" id={id}>
       <img src={img} alt={name}/>
     </div>
-    </Link>
+    </NavLink>
   )
 
 }
